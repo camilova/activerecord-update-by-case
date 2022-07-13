a9ff20d2fbf327382a8d176ef6d8513f032bf5ac
Camilo Vargas A <vargas.antiguay@gmail.com>
2022-07-13 03:09:42 -0400

    Allow values for key attr as string or symbols

--- lib/update_by_case.rb
+++ lib/update_by_case.rb
@@ -28,7 +28,11 @@ module UpdateByCase
         case_sql_fragment = "#{attribute_name}=(CASE #{key_attribute}#{SEPARATOR}"
         attribute_cases.each do |key_attribute_value, attribute_value|
           value = sql_value(attribute_value)
-          case_sql_fragment += "WHEN #{key_attribute_value} THEN #{value}#{SEPARATOR}"
+          sql_key_attribute_value = key_attribute_value
+          if key_attribute_value.is_a?(String) || key_attribute_value.is_a?(Symbol)
+            sql_key_attribute_value = "'#{key_attribute_value}'"
+          end
+          case_sql_fragment += "WHEN #{sql_key_attribute_value} THEN #{value}#{SEPARATOR}"
         end
         sql_type = sql_type(model, attribute_name)
         case_sql_fragment += "ELSE #{attribute_name} END)::#{sql_type}"
