22e7e4e638179603504680f49e1dea090fca8fad
Camilo Vargas A <vargas.antiguay@gmail.com>
2022-07-13 01:59:14 -0400

    Set fixed id for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 require_relative '../models/test_model'
 
 [
-  { value_1: 1, value_2: 'one', value_3: true },
-  { value_1: 2, value_2: 'two', value_3: false },
-  { value_1: 3, value_2: 'three', value_3: true },
+  { id: 1, value_1: 1, value_2: 'one', value_3: true },
+  { id: 2, value_1: 2, value_2: 'two', value_3: false },
+  { id: 3, value_1: 3, value_2: 'three', value_3: true },
 ].each do |attributes|
   TestModel.create!(attributes)
 end
