6093dd1be98dbb8dbfef2dcf297c414f749fafcc
Camilo Vargas A <vargas.antiguay@gmail.com>
2022-07-13 02:51:32 -0400

    Wrap all test inside a transaction and rollback at the end

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,19 @@ require "minitest/autorun"
 require_relative "../models/test_model"
 require "byebug"
 require_relative "../support/database_configuration"
+require 'minitest/hooks/test'
+require 'active_record'
 
 DatabaseConfiguration.establish_connection
+
+class GemTest < Minitest::Test
+  include Minitest::Hooks
+
+  def around
+    ActiveRecord::Base.transaction do
+      super
+      raise ActiveRecord::Rollback
+    end
+  end
+
+end
