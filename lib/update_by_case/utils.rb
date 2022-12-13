b7c99043e49101c61336981d45dff2c69fbc1890
Camilo Vargas A <vargas.antiguay@gmail.com>
2022-12-13 14:53:56 -0300

    Create dummy false case statement for empty hash cases

--- lib/update_by_case/utils.rb
+++ lib/update_by_case/utils.rb
@@ -9,9 +9,13 @@ module UpdateByCase
       def build_case_statement(model, attribute_name, key_attribute, cases)
         case_statement = Arel::Nodes::Case.new
         arel_table = model.arel_table
-        cases.each do |xcase|
-          condition = arel_table[key_attribute].eq(xcase.first)
-          case_statement.when(condition).then(xcase.second)
+        unless cases.empty?
+          cases.each do |xcase|
+            condition = arel_table[key_attribute].eq(xcase.first)
+            case_statement.when(condition).then(xcase.second)
+          end
+        else
+          case_statement.when(false).then(arel_table[attribute_name])
         end
         case_statement.else(arel_table[attribute_name])
       end
